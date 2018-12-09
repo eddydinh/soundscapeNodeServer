a1d08c55fdfc3c0d4399be97ec62137c44071c50
Eddy Dinh <minhdinh25599@gmail.com>
2018-12-09 02:54:18 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileupload = require("express-fileupload");
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(fileupload());
+app.use(cors());
+const database = {
+    markers: [
+        {
+            title: 'Sound 1',
+            description : 'Description of the sound 1',
+            lat: 49.87219002998007,
+            lng: -119.48283236669567,
+            filename:`soundeffect1.mp3`,
+            filetype:'mp3'
+            
+        },
+        
+          {
+            title: 'Sound 2',
+            description : 'Description of the sound 2',
+            lat: 49.87022622111901,
+            lng: -119.47962015357234,
+            filename:`soundeffect1.mp3`,
+            filetype:'mp3'
+            
+        }
+    ]
+}
+app.listen(3000, ()=>{
+    console.log('app is running on port 3000');
+})
+
+app.get('/', (req,res)=>{
+    res.json(database.markers);
+})
+app.get('/:fileName',function(req,res,next){
+    
+  const options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+ const fileName = req.params.fileName;
+    
+    res.sendFile(fileName,options,(err)=>{
+        if(err){
+            next(err);
+        }else{
+            console.log('Sent: ', fileName);
+        }
+    })
+});
+
+app.post('/addmarker', (req,res)=>{
+    const {title,description,lat,lng,filetype,filename} = req.body;
+
+    if(!req.files){
+        
+        res.json("File Not Found!");
+    }else{
+    let uploadFile = req.files.file;
+       uploadFile.mv(`${__dirname}/public/files/${filename}`,  (err) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        
+    //DATABASE CODE GOES HERE
+        database.markers.push({
+        title: title,
+        description: description,
+        lat: lat,
+        lng: lng, 
+        filename:filename,
+        filetype:filetype
+    
+    })
+    res.json('success');
+
+    }) 
+}  
+})
