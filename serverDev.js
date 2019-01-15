d33c85fdb4f7ecc5a7bc02b3586b67c7922748a8
Eddy Dinh <minhdinh25599@gmail.com>
2019-01-15 09:18:52 -0800

    add server for development

---
+++ serverDev.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileupload = require("express-fileupload");
+const cors = require('cors');
+const knex = require('knex')
+
+
+//db.select('*').from('markers').tjen(data => {
+//    console.log(data);
+//})
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'digmed1!',
+        database: 'soundscape'
+    }
+})
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(fileupload());
+app.use(cors());
+app.use(express.static('public'));
+
+
+
+app.listen(3000, () => {
+    console.log(`app is running on port 3000`);
+})
+
+app.get('/', (req, res) => {
+    res.json('Connected successfully');
+})
+
+app.get('/database', (req, res) => {
+    db('markers').returning('*').select('*').then(response => {
+        res.json(response);
+    }).catch(error => res.status(400).json('Unable to fetch markers information'))
+})
+
+app.get('/:fileName', function (req, res, next) {
+
+    const options = {
+        root: __dirname + '/public/files/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+    const fileName = req.params.fileName;
+
+    res.sendFile(fileName, options, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent: ', fileName);
+        }
+    })
+});
+
+app.post('/addmarker', (req, res) => {
+    const {
+        title,
+        description,
+        lat,
+        lng,
+        filetype,
+        filename
+    } = req.body;
+
+    if (!req.files) {
+
+        res.json("File Not Found!");
+    } else {
+        let uploadFile = req.files.file;
+        uploadFile.mv(`${__dirname}/public/files/${filename}`, (err) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+
+            //DATABASE CODE GOES HERE
+            db('markers').insert({
+                title: title,
+                description: description,
+                lat: lat,
+                lng: lng,
+                filename: filename,
+                filetype: filetype
+
+            }).then(console.log).catch(error => res.status(400).json('Unable to add marker'));
+            res.json('success');
+
+        })
+    }
+});
+
+app.post('/editmarker', (req, res) => {
+
+    const {
+        id, filename
+    } = req.body;
+    const obj = req.body;
+
+    if (req.files) {
+        let uploadFile = req.files.file;
+        uploadFile.mv(`${__dirname}/public/files/${filename}`, (err) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+
+
+
+        })
+    }
+
+    //DATABASE CODE GOES HERE
+    db('markers').where('id', id).update(
+        obj).then(console.log).catch(error => res.status(400).json('Unable to add marker'));
+    res.json("edit success");
+});
+
+
+app.post('/deletemarker', (req, res) => {
+    const {
+        id
+    } = req.body;
+
+
+
+    //DATABASE CODE GOES HERE
+    db('markers').where('id', id).del().then(console.log).catch(error => res.status(400).json('Unable to delete marker'));
+    res.json('delete success');
+
+});
