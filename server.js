8e88ada84a641e477dcb3d3d83349230135428cc
Eddy Dinh <minhdinh25599@gmail.com>
2019-02-24 14:52:59 -0800

    change upload limit size

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ const db = knex({
 //})
 const app = express();
 
+
 //Middle wares
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(fileupload());
 app.use(cors());
 app.use(express.static('public'));
